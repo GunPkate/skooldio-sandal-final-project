fdd9353f0702cff4050e1ef04ee76c3c004b9114
GunPKate <gunPK>
2024-03-05 11:36:01 +0700

    mycart basic layout

--- src/pages/mycart.jsx
+++ src/pages/mycart.jsx
@@ -1,7 +1,47 @@
 export default function Mycart(){
+    const items = [
+        {
+            id:1,
+            name: "item 1",
+            price: 2000
+        },
+        {
+            id:2,
+            name: "item 1",
+            price: 2000
+        },
+    ]
     return(
-        <div className="section">
-            <h1>My Cart</h1>
+        <div className="section lg:flex md:flex:none md:block">
+            <div className="itemList bg-blue-400 min-h-[80vh] min-w-[40vw]">
+                <h1>My Cart</h1>
+                {items.length >0 ? items.map( item => 
+                <div key={item.id}>
+                    <div>
+                        {item.name}
+                    </div>
+                    <div>
+                        {item.price}
+                    </div>
+                    <div>
+                        <select className="ml-2">
+                            <option>QTY</option>
+                        </select>
+                        <select className="ml-2">
+                            <option>Size</option>
+                        </select>
+                        <select className="ml-2">
+                            <option>Colors</option>
+                        </select>
+                    </div>
+                </div>
+                )
+                :<>Please Select items</>}
+            </div>
+            <div className="summary bg-red-400 min-h-[80vh] min-w-[40vw]">
+                <h1>Summary</h1>
+            </div>
+            
         </div>
     )
 }
