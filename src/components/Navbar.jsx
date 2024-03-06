9a7d603bb98a92f0692d265631f088595319114f
GunPKate <gunPK>
2024-03-06 11:46:28 +0700

    user context V1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
 import { Link } from "react-router-dom"
+import { UserContext } from "../App"
+import { useContext } from "react"
+
 
 
 export default function Navbar() {
     // console.log(userData)
-    return (<div className="bg-fixed bg-black min-h-[10vh] min-w-full">
-        <Link className="text-white" to={"/"}>
-            Home
-        </Link>
+    const userInfo = useContext(UserContext)
+
+    const navItemStyle = "text-white ml-2"
+    return (
+    <div className="bg-fixed bg-black min-h-[10vh] min-w-full flex">
+        <div className="container">
+            <Link className={navItemStyle} to={"/"}>
+                Home
+            </Link>
 
 
-        <Link className="text-white" to={"/Products"}>
-            Products
-        </Link>
+            <Link className={navItemStyle} to={"/Products"}>
+                Products
+            </Link>
 
-        <Link className="text-white" to={"/Productdetail"}>
-            Product Detail
-        </Link>
+            <Link className={navItemStyle} to={"/Productdetail"}>
+                Product Detail
+            </Link>
 
-        <Link className="text-white" to={"/Mycart"}>
-            My cart
-        </Link>
-    </div>)
+            <Link className={navItemStyle} to={"/Mycart"}>
+                My cart
+            </Link>
+        </div>
+
+        <div className="text-white bg-blue-400">Hello {userInfo.user}</div>
+    </div>
+    )
 }
