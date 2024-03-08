cdcef2ed6a89ba705f7b41ea167306e316325dec
GunPKate <gunPK>
2024-03-08 21:24:00 +0700

    my cart basic  layout PC

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,23 +25,23 @@ export default function Navbar() {
         {value:"Shoes"},
         {value:"Accessories"},
     ]
-    const navItemStyle = "text-white ml-6 text-base"
+    const navItemStyle = "text-white ml-6 text-base my-auto "
+    const contentStyle = "flex items-center text-white "
     const cart = `
     <svg width="40px" height="40px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fcfcfc" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>    `
 
     return (
-    <div className="bg-fixed bg-black h-60px  w-full flex">
-        <div className="container">
+    <div className={"bg-fixed bg-black lg:h-[60px] h-[56px]  w-full "+contentStyle}>
+        <div className="container ">
             <Link className={navItemStyle} to={"/"}>
                 Home
             </Link>
             {categories? categories.map((item,id) => 
-
-                <span key={id+1}>                        
-                    <Link  to={`/Products/?=${item.value}`} className={navItemStyle}>
+                    <Link key={id+1} to={`/Products/?=${item.value}`} className={navItemStyle}>
+                        <span>                        
                             {item.value}
+                        </span>
                     </Link>
-                </span>
 
                 ): <></>}
            {/* 
@@ -51,7 +51,7 @@ export default function Navbar() {
 
         </div>
 
-        <div className="flex text-white">
+        <div className={contentStyle}>
             {userInfo.user === "" || userInfo.user === null ?
                 <button onClick={logIn}>   Log in          </button>:
                 <button onClick={logOut}>   Log out          </button>
@@ -63,7 +63,7 @@ export default function Navbar() {
                 {userPurhcase.length}
             </span>
             <Link className={navItemStyle} to={"/Mycart"}>
-                {/* <div dangerouslySetInnerHTML={{__html: `${cart}`}}></div> */}
+                <div dangerouslySetInnerHTML={{__html: `${cart}`}}></div>
             </Link>
         </div>
     </div>
--- src/pages/mycart.jsx
+++ src/pages/mycart.jsx
@@ -13,20 +13,30 @@ export default function Mycart(){
         console.log(e)
         setuserPurhcase(tempData)
 
-    } 
+    }
+
+    const CardTemplate = ({ title, width, children, ml, mr}) => (<>
+        <div className={"itemList bg-blue-400 min-h-[80vh] "+ width + ml + mr}>
+            <h5 className="m-[24px] text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {title} </h5>
+                {children}
+        </div>
+    </>)
 
 
     return(
         <>
         <Navbar/>
+            <div className="mx-auto"> 
             <div className="min-w=[100vw] my-[40px] mx-[max(160px,16px)]">
                 <h1 className="m-[24px] text-2xl font-bold">My Cart</h1>
             </div>
             <div className="section section-mycart lg:flex md:flex:none md:block">
-                <div className="itemList bg-blue-400 min-h-[80vh] min-w-[944px] mx-[max(160px,16px)] mr-[20px]">
-                    <h5 className="m-[24px] text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Summary</h5>
+
+                <CardTemplate title={"Items"} width={"min-w-[944px]"} ml = {" mx-[max(160px,16px)] "} mr = {" mr-[20px] "}>
+
                     {userPurhcase.length >0 ? userPurhcase.map( (item,id) =>                
-                            <div key={id} className="flex flex-col items-center bg-white border border-gray-200  shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <div className="flex flex-col items-center bg-white border border-gray-200  shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <div>
                                 <img className="object-cover w-full rounded-t-lg h-[209px] md:h-auto lg:w-[209px] md:w-48 md:rounded-none md:rounded-s-lg" src={item.image} alt=""/>
                                 <div className="flex flex-col justify-between p-4 leading-normal">
 
@@ -65,60 +75,57 @@ export default function Mycart(){
                                     </div>
                                 </div>
                             </div>
-                    )
-                    :<>
-                    <div>
-                        <h1>Your cart is empty</h1>
-                        <button className="button">Continue Shooping</button>
-                    </div>
-                    </>}
-                </div>
-                <div className="summary bg-red-400 min-h-[80vh] min-w-[500px] mx-[max(160px,16px)] ml-[20px]">                  
-                    <h5 className="m-[24px] text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Summary</h5>
+                        </div>)
+
+                        :<>
+                            <div>
+                                <h1>Your cart is empty</h1>
+                                <button className="button">Continue Shooping</button>
+                            </div>
+                        </>}
+                </CardTemplate>
+
+                <CardTemplate title={"Summary"} width={"min-w-[500px]"} ml={" mx-[max(160px,16px)] "}  mr={" mr-[20px] "} >
                     <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
 
                         <div className="flex flex-col justify-between p-4 leading-normal">
 
-                            <div className="flex justify-between">
-
-
-                            </div>
-
                             <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 block">
+                                {userPurhcase.length >0 ? 
+                                    userPurhcase.map(  item => 
+                                        <div key={item.id}>{item.name} {item.qty * item.price}</div>     
+                                    ) 
+                                    :<div></div>
+                                }
             
-                            {userPurhcase.length >0 ? 
-                                userPurhcase.map(  item => 
-                                    <div key={item.id}>{item.name} {item.qty * item.price}</div>     
-                                ) 
-                                :<div></div>
-                            }
-        
-                            <h1>Subtotal 
-                                {userPurhcase.length > 1 ? 
-                                userPurhcase.reduce((u,v)=>{
-                                    return u.qty*u.price + v.qty*v.price
-                                  })
-                                : userPurhcase.length === 1 ? userPurhcase[0].price * userPurhcase[0].qty :0 }
-                            </h1>
-                            <h1>Shipping fee</h1>
-                            <h1>Total 
-                                {userPurhcase.length > 1 ? 
+                                <h1>Subtotal 
+                                    {userPurhcase.length > 1 ? 
                                     userPurhcase.reduce((u,v)=>{
                                         return u.qty*u.price + v.qty*v.price
                                     })
-                                : userPurhcase.length === 1 ? userPurhcase[0].price * userPurhcase[0].qty :0 }
+                                    : userPurhcase.length === 1 ? userPurhcase[0].price * userPurhcase[0].qty :0 }
+                                </h1>
+                                <h1>Shipping fee</h1>
+                                <h1>Total 
+                                    {userPurhcase.length > 1 ? 
+                                        userPurhcase.reduce((u,v)=>{
+                                            return u.qty*u.price + v.qty*v.price
+                                        })
+                                    : userPurhcase.length === 1 ? userPurhcase[0].price * userPurhcase[0].qty :0 }
 
-                            </h1>
-                            <button className="button">Check Out</button>
-                            <button className="button">Continue Shooping</button>
-                                    <h1>
-                                        {/* {item.price} */}
-                                    </h1>
-                                </div>
+                                </h1>
+                                <button className="button">Check Out</button>
+                                <button className="button">Continue Shooping</button>
+                                <h1>
+                                    {/* {item.price} */}
+                                </h1>
+                            </div>
                         </div>
                     </div>
+                    </CardTemplate>
 
-                </div>
+            </div>
+
             </div>
         <Footer/>
         </>
