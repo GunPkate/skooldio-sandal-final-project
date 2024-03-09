4811e67f08992943666af42832298d54aa0eb9ac
GunPKate <gunPK>
2024-03-09 18:04:33 +0700

    mycart noti

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom"
 import { UserContext } from "../../App"
 import { useContext } from "react"
-// import  "./Navbar.css"
 
 
 
@@ -29,6 +28,13 @@ export default function Navbar() {
     const contentStyle = "flex items-center text-white "
     const cart = `
     <svg width="40px" height="40px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fcfcfc" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>    `
+    const noti = `  <span style={{
+        height: "30px",
+        width: "25px",
+        background: "red",
+        borderRadius: "50%",
+        display: "inlineBlock"
+    }}>x</span>`
 
     return (
     <div className={"bg-fixed bg-black lg:h-[60px] h-[56px]  w-full "+contentStyle}>
@@ -63,7 +69,20 @@ export default function Navbar() {
                 {userPurhcase.length}
             </span>
             <Link className={navItemStyle} to={"/Mycart"}>
-                <div dangerouslySetInnerHTML={{__html: `${cart}`}}></div>
+                <div className="flex">
+                    <span dangerouslySetInnerHTML={{__html: `${cart}`}}></span>
+                    {userPurhcase.length>0?
+                    <span style={{
+                        height: "15px",
+                        width: "15px",
+                        background: "red",
+                        borderRadius: "50%",
+                        display: "inlineBlock",
+                        color:"red",
+                        overflow: "hidden"
+                    }}>x</span>
+                    :<></>}
+                </div>
             </Link>
         </div>
     </div>
