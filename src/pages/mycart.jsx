bd4c411468d5be5017d0ea12d7690d1e188f54f3
GunPKate <gunPK>
2024-03-09 09:49:12 +0700

    resposive V1

--- src/pages/mycart.jsx
+++ src/pages/mycart.jsx
@@ -15,9 +15,10 @@ export default function Mycart(){
 
     }
 
+    const marginStyle = " lg: m-[24px] "
     const CardTemplate = ({ title, width, children, ml, mr}) => (<>
         <div className={"itemList bg-blue-400 min-h-[80vh] "+ width + ml + mr}>
-            <h5 className="m-[24px] text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {title} </h5>
+            <h5 className={marginStyle+ " text-2xl font-bold tracking-tight text-gray-900 dark:text-white"}> {title} </h5>
                 {children}
         </div>
     </>)
@@ -27,52 +28,50 @@ export default function Mycart(){
         <>
         <Navbar/>
             <div className="mx-auto"> 
-            <div className="min-w=[100vw] my-[40px] mx-[max(160px,16px)]">
-                <h1 className="m-[24px] text-2xl font-bold">My Cart</h1>
+            <div className="min-w=[100vw] my-[40px] mx-[max(8.34%,16px)]">
+                <h1 className={marginStyle+ " text-2xl font-bold"}>My Cart</h1>
             </div>
             <div className="section section-mycart lg:flex md:flex:none md:block">
 
-                <CardTemplate title={"Items"} width={"min-w-[944px]"} ml = {" mx-[max(160px,16px)] "} mr = {" mr-[20px] "}>
+                <CardTemplate title={"Items"} width={"min-w-[49.16%]"} ml = {" mx-[max(16px,16px)] lg:ml-[max(8.34%,16px)] "} mr = {" mr-[20px] "}>
 
                     {userPurhcase.length >0 ? userPurhcase.map( (item,id) =>                
-                        <div className="flex flex-col items-center bg-white border border-gray-200  shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                            <div>
-                                <img className="object-cover w-full rounded-t-lg h-[209px] md:h-auto lg:w-[209px] md:w-48 md:rounded-none md:rounded-s-lg" src={item.image} alt=""/>
-                                <div className="flex flex-col justify-between p-4 leading-normal">
+                        <div key={id} className="flex inline-block">
+                            <img className={marginStyle+ "object-cover rounded-t-lg h-[209px] md:h-auto lg:w-[209px] md:w-48 md:rounded-none md:rounded-s-lg"} src={item.image} alt=""/>
+                            <div className="flex flex-col justify-between p-4 leading-normal mx-auto">
 
-                                    <div className="flex justify-between">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
-                                    <button className="bg-red-300" onClick={(e)=>{handleDelete(item)}}> delete </button>
-                                    </div>
+                                <div className="flex justify-between">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
+                                <button className="bg-red-300" onClick={(e)=>{handleDelete(item)}}> delete </button>
+                                </div>
 
-                                    <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
-                                        <div className="ml-2">
-                                            <option>QTY</option>
-                                        </div>
-                                        <div className="ml-2">
-                                            <option>Size</option>
-                                        </div>
-                                        <div className="ml-2">
-                                            <option>Colors</option>
-                                        </div>
-                                        <h1 style={{opacity: 0.0}}>
-                                            {item.price}
-                                        </h1>
+                                <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
+                                    <div className="ml-2">
+                                        <option>QTY</option>
                                     </div>
-                                    <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
-                                        <select className="ml-2">
-                                            <option>QTY</option>
-                                        </select>
-                                        <select className="ml-2">
-                                            <option>Size</option>
-                                        </select>
-                                        <select className="ml-2">
-                                            <option>Colors</option>
-                                        </select>
-                                        <h1>
-                                            {item.price * item.qty}
-                                        </h1>
+                                    <div className="ml-2">
+                                        <option>Size</option>
                                     </div>
+                                    <div className="ml-2">
+                                        <option>Colors</option>
+                                    </div>
+                                    <h1 style={{opacity: 0.0}}>
+                                        {item.price}
+                                    </h1>
+                                </div>
+                                <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-between">
+                                    <select className="ml-2">
+                                        <option>QTY</option>
+                                    </select>
+                                    <select className="ml-2">
+                                        <option>Size</option>
+                                    </select>
+                                    <select className="ml-2">
+                                        <option>Colors</option>
+                                    </select>
+                                    <h1>
+                                        {item.price * item.qty}
+                                    </h1>
                                 </div>
                             </div>
                         </div>)
@@ -85,7 +84,7 @@ export default function Mycart(){
                         </>}
                 </CardTemplate>
 
-                <CardTemplate title={"Summary"} width={"min-w-[500px]"} ml={" mx-[max(160px,16px)] "}  mr={" mr-[20px] "} >
+                <CardTemplate title={"Summary"} width={"min-w-[32.08%]"} mr={"  "}  ml={" lg:ml-[20px] "} >
                     <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
 
                         <div className="flex flex-col justify-between p-4 leading-normal">
