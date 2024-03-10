0ea454c61d618edff795292a6c95fc508822916f
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-03-10 14:12:11 +0700

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
         primary: "#def81c",
         "primary-700": "#C1CD00",
