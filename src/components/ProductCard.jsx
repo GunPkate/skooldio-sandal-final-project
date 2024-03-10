cd57d77d142f7be3786614f2a379142f72b59d05
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-03-10 15:45:15 +0700

    fetch price, add more items

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ const ProductsDetail = [
     name: "Reyon Long Sleeve Shirt",
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
     description: "Product description",
-    rating: 3,
+    rating: 5,
     discount: true,
     originalPrice: 2000,
     price: 1000,
@@ -23,10 +23,41 @@ const ProductsDetail = [
     description: "Product description",
     rating: 3,
     discount: true,
+    originalPrice: 3000,
+    price: 1000,
+  },
+  {
+    name: "Reyon Long Sleeve Shirt",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    description: "Product description",
+    rating: 1,
+    discount: true,
     originalPrice: 2000,
     price: 1000,
   },
+  {
+    name: "Boxy Tailored Jacket",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    description: "Product description",
+    rating: 2,
+    discount: true,
+    originalPrice: 2000,
+    price: 1900,
+  },
+  {
+    name: "Premium Linen Long Sleeve",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    description: "Product description",
+    rating: 4,
+    discount: false,
+    originalPrice: 3000,
+    price: 1000,
+  },
 ];
+
+function numberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const createStars = (amount) => {
   return amount ? [...Array(amount)].map(() => <span>⭐</span>) : null;
@@ -35,13 +66,24 @@ const createStars = (amount) => {
 function ProductCard() {
   return ProductsDetail.map((item) => {
     return (
-      <div className="mb-10" key={item.name}>
-        <img
-          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          className="h-[370px] sm:min-w-[340px] 2xl:min-w-[370px] object-cover mb-6"
-          alt={`${item.name}`}
-        />
-        <div className="grid grid-cols-1 gap-2 sm:w-[340px] md:w-[370px]">
+      <div className="mb-10 2xl:mb-0" key={item.name}>
+        <div className="relative">
+          {item.discount && (
+            <p className="font-light text-white py-1 px-[10px] bg-danger w-fit absolute top-6 right-0">
+              -
+              {Math.round(
+                ((item.originalPrice - item.price) / item.originalPrice) * 100
+              )}
+              %
+            </p>
+          )}
+          <img
+            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            className="h-[370px] min-w-[340px] 2xl:min-w-[370px] object-cover mb-6"
+            alt={`${item.name}`}
+          />
+        </div>
+        <div className="grid grid-cols-1 gap-2 w-[340px] 2xl:w-[370px]">
           <h2 className="font-bold text-2xl text-secondary whitespace-nowrap text-ellipsis overflow-hidden">
             {item.name}
           </h2>
@@ -53,14 +95,16 @@ function ProductCard() {
             {item.discount ? (
               <div className="flex items-center gap-4 justify-end">
                 <p className="font-semibold text-sm line-through text-secondary-700">
-                  OG price
+                  {numberWithCommas(item.originalPrice)}
                 </p>
-                <p className="font-bold text-2xl text-danger">Discount Price</p>
+                <p className="font-bold text-2xl text-danger">
+                  THB {numberWithCommas(item.price)}
+                </p>
               </div>
             ) : (
               <div className="flex items-center gap-4 justify-end">
                 <p className="font-bold text-2xl text-secondary">
-                  Discount Price
+                  THB {numberWithCommas(item.price)}
                 </p>
               </div>
             )}
