524b4e251a64fba019f44b86af9f46c336ba52c4
GunPKate <gunPK>
2024-03-13 00:01:45 +0700

    Revert "UserContext"
    This reverts commit 5b0a3564ad5b9d605dd1a5f1cc8419e49c9d2484.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Products from "./pages/Products";
@@ -6,9 +6,7 @@ import Homepage from "./pages/Homepage";
 import Mycart from "./pages/Mycart";
 import ProductDetail from "./pages/ProductDetail";
 
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import UserContext from "./components/UserContext";
 
 const router = createBrowserRouter([
   {
@@ -31,9 +29,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserContext>
-      <RouterProvider router={router} />
-    </UserContext>
-
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
