6af51f5cf0e449995d3154825e4ef167ba487b97
sirawee'14 <pasika.swn@gmail.com>
2024-03-20 17:10:03 +0700

    add Modal component

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import Product from "../components/ProductDetailComponents/Product";
+// import Modal from "../components/ProductDetailComponents/Modal/Modal";
 
 export default function ProductDetail() {
   return (
     <div className="section">
       <Navbar />
       <Product />
+      {/* <Modal /> */}
       <Footer />
     </div>
   );
