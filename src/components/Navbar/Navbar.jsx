fd4222f658c0447d5d1daafa27ef99840b2ff586
GunPKate <gunPK>
2024-03-21 21:57:45 +0700

    responvie navbar menuv1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,9 +18,12 @@ export default function Navbar() {
         categories
     } = useContext(UserContext)
 
-    const navHomeStyle = "text-white text-base my-auto "
-    const navItemStyle = "text-white ml-6 text-base my-auto "
+    const navHomeStyle = " text-white text-base my-auto "
+    const navItemStyle = "text-white ml-6 text-base my-auto bg-black"
     const contentStyle = "flex items-center text-white "
+    const responsiveStyle = "invisible md:visible md:opacity-1 md:items-center   "
+
+    const baseMenuStyle = "flex md:inline-flex bg-black "
     const cart = `
     <svg width="40px" height="40px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fcfcfc" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>    `
     const noti = `  <span style={{
@@ -38,7 +41,7 @@ export default function Navbar() {
                 Home
             </Link>
             {categories? categories.map((item,id) => 
-                <span className="flex md:inline-flex"> 
+                <span className={baseMenuStyle + responsiveStyle}> 
                     <Link key={id+1} to={`/Products/${item.name}/${item.permalink}`} className={navItemStyle}>
                         {item.name}
                     </Link>
