aabd7d4dd5b7b3a3f102da0dafa4d63817ff4d26
bboworn <bowornphakz@gmail.com>
2024-03-23 16:55:46 +0700

    homepage almost finished

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -43,64 +43,83 @@ function Homepage() {
         />
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center gap-10 mx-auto h-fit mb-16">
-        <div className="flex flex-col w-full md:w-[363px] h-96">
-          <p className="font-bold text-4xl md:text-8xl">2024</p>
-          <p className="font-bold text-3xl md:text-5xl">Collection</p>
-          <p className="text-base">
-            Step into a world of winter elegance and style with our latest
-            Winter Collection. As temperatures drop, our curated selection of
-            clothing is designed to keep you fashionably warm. From luxurious
-            knitwear to trend-setting outerwear, each piece in our collection is
-            a celebration of seasonal sophistication. Explore the blend of
-            comfort and fashion, as we present you with the must-have ensembles
-            to make a statement in the chilly months ahead. Welcome to a winter
-            wardrobe that seamlessly combines coziness with chic aesthetics.
-          </p>
-        </div>
-        <div className="relative">
-          <img
-            className="w-full md:w-[575px] h-[500px] object-cover bg-gradient-to-t from-slate-900 to-transparent "
-            src="./src/assets/img2.jpg"
-            alt="Cozy Breeze"
-          />
-          <div className="absolute bottom-4 px-4 flex flex-col items-center text-white ">
-            <p className="font-bold text-xl md:text-2xl">Cozy Breeze</p>
-            <p className="font-normal text-base">
-              Embrace the season with our carefully curated selection of
-              garments, each piece thoughtfully designed to blend fashion and
-              functionality. From cozy knits to elegant outerwear, our
-              collection invites you to indulge in the allure of winter fashion.
-            </p>
-            <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
-              View more
-            </button>
+      <div className="gap-10">
+  <div className="relative bg-white px-6 pb-20 lg:px-8 lg:pb-28">
+    <div className="mx-auto max-w-7xl">
+      <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
+        <div className="flex flex-col overflow-hidden rounded-lg">
+          <div className="flex flex-1 flex-col justify-between bg-white p-6">
+            <div>
+              <p className="text-6xl font-bold text-black">
+                2024 Collection
+              </p>
+              <p className="mt-3 text-base text-black">
+                Step into a world of winter elegance and style with our latest
+                Winter Collection. As temperatures drop, our curated selection
+                of clothing is designed to keep you fashionably warm. From
+                luxurious knitwear to trend-setting outerwear, each piece in
+                our collection is a celebration of seasonal sophistication.
+                Explore the blend of comfort and fashion, as we present you
+                with the must-have ensembles to make a statement in the chilly
+                months ahead. Welcome to a winter wardrobe that seamlessly
+                combines coziness with chic aesthetics.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="relative">
+
+        <div className="flex flex-col overflow-hidden relative">
           <img
-            className="w-full md:min-w-[575px] h-[500px] object-cover bg-gradient-to-t from-slate-900 to-transparent "
-            src="./src/assets/img3.jpg"
-            alt="Flexi Move"
+            className="object-fit"
+            src="https://s3-alpha-sig.figma.com/img/d5fe/7eb0/680055de67c3d9c6a271ba03847a3cb9?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QoAoFPFXTGXqVstIsmBMC1Sq3~qsDWeK3I-z3NwP12WOlKPS1Lwe7KRuHjvCBR-cCyxGKAz4wT~tr3KbIMlOZvxVxhTwJspMskLIvd3T24u6GCoZdC4bMjvqCK9AvKo6DoAIgNS1fETMpXgo0ICafS1TR~hg0S2t~hSaZFFlqxpAlo3D6zOREfaMLdPq1GHk1Vj6dsp2QZTpDtwta35OnVf0tc2oj6WuPLBV81ZOYB6IYs3Gig6MczVVDGFoFJz9sxv6iDbh6J4xmTM7lDg9RK8WzVjY91CoOGJRgUDi-fITZ3-OmqgJqx9ymptp5Ki-HNTjerkv5KwkvZBV~zvsFg__"
+            alt=""
           />
-          <div className="absolute bottom-4 px-4 flex flex-col items-center text-white ">
-            <p className="font-bold text-xl md:text-2xl">Flexi Move</p>
-            <p className="font-normal text-base">
-              Step into a world where fashion meets functionality with our
-              latest Sneaker Collection. Designed for those who appreciate the
-              perfect fusion of style and comfort, our curated selection of
-              sneakers is a celebration of urban chic.
-            </p>
-            <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
-              View more
-            </button>
+          <div className="flex flex-1 flex-col justify-between p-6 absolute bottom-0">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-xl font-semibold text-white">Cozy Breeze</p>
+              <p className="mt-3 text-base text-white font-light">
+                Embrace the season with our carefully curated selection of
+                garments, each piece thoughtfully designed to blend fashion and
+                functionality. From cozy knits to elegant outerwear, our
+                collection invites you to indulge in the allure of winter
+                fashion.
+              </p>
+              <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
+                View more
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col overflow-hidden relative">
+          <img
+            className="w-full object-fit"
+            src="https://s3-alpha-sig.figma.com/img/1f4b/d42b/1a0763187b13c1a39bff2d14eb960993?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gEKex1PxYViM7KPPxulfJklZIC7Mqmdn8aQ6mBZNzy7AKrkbgGkbR94W7S7yNlny2yt6SDI4beAzXimPDrZzokvYQtk0kbzz6BLH-bqbUs4sYJ5VEv6GDG8sfGDrFxAVqIKnUgh-gIlDNpTG4NmW8IQGBdtYwd0uoxL5pOeUo8lbUZ593oHFhTEYDaeCSKLFv8WSaAU~sMlkM1clnUVXVQtOB3mxMYObWcHZsanK~6pK~Ar5Nimju5y5n~4xgj-9tqyQqqU6EHoNDKB4SqbPSAIkFlXG-s1BtVx-UMVtsTZPWOyxsUu34J7eU6IWo-RLEL6ZG86---8-EiXLmX70Jw__"
+            alt=""
+          />
+          <div className="flex flex-1 flex-col justify-between p-6 absolute bottom-0">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-xl font-semibold text-white">Flexi Move</p>
+              <p className="mt-3 text-base text-white font-light">
+                Step into a world where fashion meets functionality with our
+                latest Sneaker Collection. Designed for those who appreciate
+                the perfect fusion of style and comfort, our curated selection
+                of sneakers is a celebration of urban chic.
+              </p>
+              <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded">
+                View more
+              </button>
+            </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>
+</div>     
 
       <div className="flex flex-col items-center px-4 font-bold mb-16 mx-auto">
         <p className="text-[32px] mb-16">Feature Products</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 mx-auto w-full md:w-fit">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 mx-auto w-full md:w-fit">
           {items}
         </div>
       </div>
