6e586b6b3c698c0ba0d509b2a9a94d3a38e42fb3
GunPKate <gunPK>
2024-03-27 09:02:07 +0700

    set loading in middle

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className="w-full flex justify-center h-full">
+    <div className="w-full flex justify-center min-h-screen">
       <span className="loading loading-spinner loading-lg text-primary"></span>
     </div>
   );
