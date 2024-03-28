75882515499ba35a2410ec076879c15c3995532e
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-03-28 22:32:21 +0700

    "fix modal"

--- src/components/ProductDetailComponents/ProductDetailRight.jsx
+++ src/components/ProductDetailComponents/ProductDetailRight.jsx
@@ -6,7 +6,8 @@ import Dropdown from "./Dropdown";
 import Modal from "./Modal";
 
 // price with commas from k'Ter (product cart)
-export function numberWithCommas(number) {
+export function numberWithCommas(num) {
+  const number = (Math.round(num * 100) / 100).toFixed(2);
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
@@ -42,7 +43,6 @@ const ProductDetailRight = (data) => {
       setReadOnly(false);
     }
   }, []);
-  
 
   // Function to calculate the sum of "remains" values
   function sumRemains(variants) {
@@ -52,7 +52,6 @@ const ProductDetailRight = (data) => {
     }
     return sum;
   }
-  
 
   // function to handle size selection
   const handleSizeSelection = (size) => {
@@ -88,8 +87,6 @@ const ProductDetailRight = (data) => {
 
     setRemains(remains);
   };
-
-  
 
   //rating section from k'Ter (product cart)
   const createStars = (rating) => {
@@ -285,7 +282,7 @@ const ProductDetailRight = (data) => {
           <span className="text-red-500 font-semibold text-xl">{`(In stock : ${remains})`}</span>
         </div>
         {/* dropdown to select Qty */}
-        <Dropdown onQuantityChange={onQuantityChange} remains={remains}/>
+        <Dropdown onQuantityChange={onQuantityChange} remains={remains} />
       </div>
 
       {/* Add to cart button */}
@@ -306,7 +303,11 @@ const ProductDetailRight = (data) => {
         </Link>
       )} */}
       <button
-        className={`flex justify-center items-center w-full h-[54px] ${remains === 0 && readOnly === true ? "bg-secondary opacity-50" : "bg-black"} text-white`}
+        className={`flex justify-center items-center w-full h-[54px] ${
+          remains === 0 && readOnly === true
+            ? "bg-secondary opacity-50"
+            : "bg-black"
+        } text-white`}
         disabled={remains === 0 && readOnly === true ? true : false}
         onClick={(event) => {
           event.preventDefault();
@@ -316,11 +317,25 @@ const ProductDetailRight = (data) => {
         Add to cart
       </button>
 
-        
-      {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} selectedData={uniqueDataSize}   modalItems={{quantity:quantity, selectColor:selectColor, selectedSize:selectedSize, nameModal:data.name ,imgModal:data.imageUrls[0], priceModal:data.promotionalPrice<data.price? data.promotionalPrice : data.price}}/>}
+      {isModalOpen && (
+        <Modal
+          onClose={() => setIsModalOpen(false)}
+          selectedData={uniqueDataSize}
+          modalItems={{
+            quantity: quantity,
+            selectColor: selectColor,
+            selectedSize: selectedSize,
+            nameModal: data.name,
+            imgModal: data.imageUrls[0],
+            priceModal:
+              data.promotionalPrice < data.price
+                ? data.promotionalPrice
+                : data.price,
+          }}
+        />
+      )}
     </div>
   );
 };
 
-export default  ProductDetailRight;
-
+export default ProductDetailRight;
